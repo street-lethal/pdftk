a695a1b909c0c1d6d69e059d833c4c972b9a35a9
street-lethal <street.lethal.57@gmail.com>
2024-06-21 23:49:54 +0900

    fix filename with spaces

--- scripts_in_docker/add_meta.sh
+++ scripts_in_docker/add_meta.sh
@@ -3,7 +3,7 @@ ls  *.pdf > filenames.txt
 while read filename
 do
   export without_ext=$(echo $filename | sed -e "s/\.pdf\$//")
-  pdftk $filename update_info_utf8 $without_ext.txt output "$without_ext"_meta.pdf
+  pdftk "$filename" update_info_utf8 "$without_ext.txt" output "$without_ext"_meta.pdf
   chown 1000:1000 "$without_ext"_meta.pdf
 done < filenames.txt
 rm filenames.txt
