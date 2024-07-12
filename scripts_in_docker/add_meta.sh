b52459e0106a9ce9c63c82d7ba593781b9b34537
street-lethal <street.lethal.57@gmail.com>
2024-07-13 01:42:20 +0900

    fix script

--- scripts_in_docker/add_meta.sh
+++ scripts_in_docker/add_meta.sh
@@ -3,7 +3,11 @@ ls  *.pdf > filenames.txt
 while read filename
 do
   export without_ext=$(echo $filename | sed -e "s/\.pdf\$//")
-  pdftk "$filename" update_info_utf8 "$without_ext.txt" output "$without_ext"_meta.pdf
-  chown 1000:1000 "$without_ext"_meta.pdf
+  cp "$filename" tmp_input.pdf
+  cp "$without_ext.txt" tmp_input.txt
+  pdftk tmp_input.pdf update_info_utf8 tmp_input.txt output output.pdf
+  chown 1000:1000 output.pdf
+  mv output.pdf "$without_ext"_meta.pdf
+  rm tmp_input.*
 done < filenames.txt
 rm filenames.txt
